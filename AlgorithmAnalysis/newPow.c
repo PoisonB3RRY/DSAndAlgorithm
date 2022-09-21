09e92d5fb6d8feac1490f3850e3d469c11d377dc
nickj <nickjianjian@outlook.com>
2022-09-21 20:49:35 +0800

    format newPow

--- AlgorithmAnalysis/newPow.c
+++ AlgorithmAnalysis/newPow.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
-long int Pow(int,int);
+long int Pow(int, int);
 bool IsEven(int);
 
 void main()
 {
-    long int result = Pow(2,10);
-    printf("%ld\n",result);
+    long int result = Pow(2, 10);
+    printf("%ld\n", result);
 }
 
 long int Pow(int X, int N)
@@ -15,22 +15,22 @@ long int Pow(int X, int N)
     //其他情况将分为偶数次幂与奇数次幂
     //(1)偶数：X^N=X^(N/2)*X^(N/2)
     //(2)奇数：X^N=X^((N-1)/2)*X^((N-1)/2)*X
-    
-    if(N==0)
+
+    if (N == 0)
     {
-       return 1;
+        return 1;
     }
-    else if(N==1)
+    else if (N == 1)
     {
-      return X;
+        return X;
     }
-    else if(IsEven(X))
+    else if (IsEven(X))
     {
-        return Pow(X,N/2)*Pow(X,N/2);
+        return Pow(X, N / 2) * Pow(X, N / 2);
     }
     else
     {
-        return Pow(X,(N-1)/2)*Pow(X,(N-1)/2)*X;
+        return Pow(X, (N - 1) / 2) * Pow(X, (N - 1) / 2) * X;
     }
 }
 
@@ -38,7 +38,7 @@ bool IsEven(int x)
 {
     bool evenflag;
 
-    if(x%2==0)
+    if (x % 2 == 0)
     {
         evenflag = true;
     }
