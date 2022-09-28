b00339c04f02b223eaf79256f9c861a73cda8f14
nickj <nickjianjian@outlook.com>
2022-09-29 00:37:30 +0800

    update listImpl.c

--- tablesAndStacks/listImpl.c
+++ tablesAndStacks/listImpl.c
@@ -13,7 +13,7 @@ struct Node
 /* Return true if list is empty */
 int isEmpty(List l)
 {
-    return L->next == Null;
+    return L->next == NULL;
 }
 
 /* Return true if P is the last position in List L */
@@ -21,5 +21,20 @@ int isEmpty(List l)
 
 int isLast(Position p, List l)
 {
-    return P->next == null;
+    return P->next == NULL;
+}
+
+/* Return Position of X in List L; Null if not found */
+
+Position Find(ElementType X, List l)
+{
+    Position p;
+
+    p = l->next;
+    while (p != NULL && p->element != X)
+    {
+        p = p->next;
+    }
+
+    return p;
 }
